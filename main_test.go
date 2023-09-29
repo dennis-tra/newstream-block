e13c8b3e6a2ce9a5b9021b70061870865db654d4
Dennis Trautwein <git@dtrautwein.eu>
2023-09-29 16:36:52 +0200

    WIP

--- main_test.go
+++ main_test.go
@@ -5,13 +5,10 @@ import (
 	"fmt"
 	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p/core/host"
-	"github.com/libp2p/go-libp2p/core/network"
 	"github.com/libp2p/go-libp2p/core/peerstore"
-	ma "github.com/multiformats/go-multiaddr"
 	"github.com/stretchr/testify/require"
 	"sync"
 	"testing"
-	"time"
 )
 
 func newHost(t *testing.T) host.Host {
@@ -28,37 +25,11 @@ func newHost(t *testing.T) host.Host {
 	return h
 }
 
-func TestListenCloseCount3(t *testing.T) {
+func TestBlock(t *testing.T) {
 	for i := 0; i < 100; i++ {
 		t.Run(fmt.Sprintf("[%d]", i), func(t *testing.T) {
 			h1 := newHost(t)
 			h2 := newHost(t)
-			fmt.Println(time.Now().Format(time.RFC3339Nano)+" H1:", h1.ID(), h1.Addrs())
-			fmt.Println(time.Now().Format(time.RFC3339Nano)+" H2:", h2.ID(), h2.Addrs())
-
-			h1.Network().Notify(&network.NotifyBundle{
-				ListenF: func(n network.Network, multiaddr ma.Multiaddr) {
-					fmt.Println(time.Now().Format(time.RFC3339Nano)+" H1 started listening on:", multiaddr)
-				},
-				ConnectedF: func(n network.Network, conn network.Conn) {
-					if conn.RemotePeer() != h2.ID() {
-						panic("aahh")
-					}
-					fmt.Println(time.Now().Format(time.RFC3339Nano) + " H1 connected to: H2")
-				},
-			})
-
-			h2.Network().Notify(&network.NotifyBundle{
-				ListenF: func(n network.Network, multiaddr ma.Multiaddr) {
-					fmt.Println(time.Now().Format(time.RFC3339Nano)+" H2 started listening on:", multiaddr)
-				},
-				ConnectedF: func(n network.Network, conn network.Conn) {
-					if conn.RemotePeer() != h1.ID() {
-						panic("aahh")
-					}
-					fmt.Println(time.Now().Format(time.RFC3339Nano) + " H2 connected to: H1")
-				},
-			})
 
 			h1.Peerstore().AddAddrs(h2.ID(), h2.Addrs(), peerstore.PermanentAddrTTL)
 			h2.Peerstore().AddAddrs(h1.ID(), h1.Addrs(), peerstore.PermanentAddrTTL)
@@ -67,17 +38,14 @@ func TestListenCloseCount3(t *testing.T) {
 			wg.Add(2)
 			go func() {
 				defer wg.Done()
-				fmt.Println(time.Now().Format(time.RFC3339Nano) + " H1 -> H2 NewStream")
 				_, _ = h1.NewStream(context.Background(), h2.ID(), "/any/protocol")
-				fmt.Println(time.Now().Format(time.RFC3339Nano) + " H1 -> H2 NewStream Done!")
 			}()
 
 			go func() {
 				defer wg.Done()
-				fmt.Println(time.Now().Format(time.RFC3339Nano) + " H2 -> H1 NewStream")
 				_, _ = h2.NewStream(context.Background(), h1.ID(), "/any/protocol")
-				fmt.Println(time.Now().Format(time.RFC3339Nano) + " H2 -> H1 NewStream Done!")
 			}()
+
 			wg.Wait()
 		})
 	}
